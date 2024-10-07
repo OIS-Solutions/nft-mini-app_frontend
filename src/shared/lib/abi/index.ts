4bff656a8ca0bbc4cd915a687df96ce8cd85e268
Maxim Mikhaylenko <maximmhl@mail.ru>
2024-10-07 15:01:42 +0700

    delete others

--- src/shared/lib/abi/index.ts
+++
@@ -1,6 +0,0 @@
-import { Abi } from "viem"
-import nftContract from "./json/nftContract.json"
-
-export const abi = {
-    nftContract: nftContract as Abi,
-}
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,1 @@
-import { UseReadContractReturnType, UseReadContractsReturnType } from "wagmi"
-
 export type TAddress = `0x${string}`
