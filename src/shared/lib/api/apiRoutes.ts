ff5ef17abe3040cdcb2288603493f7b496818ce1
Maxim Mikhaylenko <maximmhl@mail.ru>
2024-10-15 17:03:37 +0700

    change api routes

--- src/shared/lib/api/apiRoutes.ts
+++ src/shared/lib/api/apiRoutes.ts
@@ -3,22 +3,16 @@ export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 export const apiEndPoint = '/api'
 
 export const apiRoutes = {
-	auth: '/api/user',
+	auth: 'api/user',
 	nft: {
-		serverRoute: '/nft',
-		baseRoute: '/api/nft',
-		mint: '/api/nft/mint',
+		serverRoute: 'nft',
+		baseRoute: 'api/nft',
+		mint: 'api/nft/mint',
 	},
 	imageApi: {
 		baseRoute: "/api/upload",
 	},
 	nftItemApi: {
 		baseRoute: "/api/uri",
-	}
-}
-
-export const apiPublicRoutes = {
-	contracts: {
-		baseRoute: '/tokensale-investment/',
 	}
 }
